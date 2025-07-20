ebc31aa5b7940b3ced0edd2e5774de393936d6fd
Mahmoud Ragab <maaahmoudragab@gmail.com>
2025-07-20 17:18:52 +0300

    resolve build 2

--- src/app/details/[media_type]/[id]/page.tsx
+++ src/app/details/[media_type]/[id]/page.tsx
@@ -1,18 +1,14 @@
 import { notFound } from "next/navigation";
 import FetchDetails from "@/data/single_requests/fetch_details";
 import DetailsContent from "@/components/details";
+import { MediaItem } from "@/data/HandleRequests";
 
-type Props = {
-  params: {
-    id: string;
-    media_type: "movie" | "tv";
-  };
-};
+export default async function MediaDetailsPage({ params }: { params: Promise<{ id: string; media_type: string }> }) {
+  const { id, media_type } = await params;
 
-export default async function MediaDetailsPage({ params }: Props) {
-  const { id, media_type } = params;
+  if (media_type !== "movie" && media_type !== "tv") return notFound();
 
-  const data = await FetchDetails(id, media_type);
+  const data: MediaItem | null = await FetchDetails(id, media_type as "movie" | "tv");
 
   if (!data) return notFound();
 
