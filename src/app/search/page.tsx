bdd1d600310f97d6017eac4701f1e9007026ad57
Mahmoud Ragab <maaahmoudragab@gmail.com>
2025-08-01 01:52:42 +0300

    resolve error

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -18,7 +18,7 @@ export default async function SearchPage({ searchParams }: { searchParams: Promi
       <Navbar />
       <main className="pt-16 md:pt-24 mx-4 md:mx-8 flex flex-col gap-3 md:gap-6">
         <div className="px-2 md:px-4 py-2 md:py-4 bg-[#ffffff1a] border-1 rounded-xl">
-          <Title text={`نتائج البحث عن : ${query}`} className="mb-4" />
+          <Title className="mb-4">{`نتائج البحث عن : ${query}`}</Title>
           <SearchList query={query} />
         </div>
       </main>
